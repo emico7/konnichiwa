53a4a488375208d2e18df0870ff4ad2dbb9240ec
emikod@mac.com <emikod@mac.com>
2016-08-14 18:37:08 -0600

    Ran rails g devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # authenticated :user do
+  #   root 'secret#index', as: :authenticated_root
+  # end
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
