b0c7d77b00a11b99ccb4618a8c2f93f1394d5dac
emikod@mac.com <emikod@mac.com>
2016-08-14 20:24:06 -0600

    Added username to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   before_action :authenticate_user!
+  protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
 end
