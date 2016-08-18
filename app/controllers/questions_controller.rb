f0403ed3a03bf6bdae390bcbb013839b1a0e130f
emikod@mac.com <emikod@mac.com>
2016-08-18 00:03:10 -0600

    Added create & edit actions, one test failing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,32 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      flash[:notice] = "Question was saved successfully."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      redirect_to :new
+    end
   end
 
   def edit
+    @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -29,18 +29,78 @@ RSpec.describe QuestionsController, type: :controller do
   #   end
   # end
   #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+
+  describe "GET new" do
+
+    before do
+      sign_in user
+    end
+
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+
+    it "instantiates @question" do
+      get :new
+      expect(assigns(:question)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+
+    before do
+      sign_in user
+    end
+
+    it "increases the number of Question by 1" do
+      expect{post :create, params: { question: { title: question.title, body: question.body } } }.to change(Question,:count).by(1)
+    end
+
+
+    it "assigns the new question to @question" do
+      post :create, params: { question: { title: question.title, body: question.body } }
+      expect(assigns(:question)).to eq Question.last
+    end
+
+
+    it "redirects to the question post" do
+      post :create, params: { question: { title: question.title, body: question.body } }
+      expect(response).to redirect_to Question.last
+    end
+  end
+
+  describe "GET edit" do
+
+    before do
+      sign_in user
+    end
+
+     it "returns http success" do
+       get :edit, {id: question.id}
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #edit view" do
+       get :edit, {id: question.id}
+       expect(response).to render_template :edit
+     end
+
+     it "assigns question to be updated to @question" do
+       get :edit, {id: question.id}
+
+       question_instance = assigns(:question)
+
+       expect(question_instance.id).to eq question.id
+       expect(question_instance.title).to eq question.title
+       expect(question_instance.body).to eq question.body
+     end
+  end
 
 end
