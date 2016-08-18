5073a8f9afc697786d61b03052e27af09cb6cdaa
emikod@mac.com <emikod@mac.com>
2016-08-18 15:11:09 -0600

    Completed CRUD for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @questions = Question.all
   end
@@ -25,6 +27,31 @@ class QuestionsController < ApplicationController
 
   def edit
     @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.assign_attributes(question_params)
+
+    if @question.save
+      flash[:notice] = "Question was updated successfully."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error updating the question. Please try again."
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+    else
+      flash.now[:alert] = "There was an error deleting the question."
+      render :show
+    end
   end
 
   private
