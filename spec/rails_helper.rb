9fdf6d32300a69da6c4800d1ed9b92f59392780b
emikod@mac.com <emikod@mac.com>
2016-08-21 18:59:17 -0600

    Added database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'support/controller_helpers'
+require 'support/database_cleaner'
+# require 'database_cleaner'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -28,6 +30,7 @@ require 'support/controller_helpers'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
@@ -37,7 +40,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
